cb2f0d779a109f829c5364b4734772371652bcac
Masoud <hassanzadeh.m@nilva.com>
2025-07-03 21:55:25 +0330

    Add nilva storage

--- next.config.mjs
+++ next.config.mjs
@@ -18,7 +18,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'via.placeholder.com',
+                hostname: 'nilva-storage.darkube.app',
                 port: '',
                 pathname: '/**',
             },
