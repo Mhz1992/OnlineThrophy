c97ad0600f0ac8f37a381026e812bc6193a89f94
Masoud <hassanzadeh.m@nilva.com>
2025-07-07 02:01:46 +0330

    Fix logout

--- src/features/auth/api/logout/api.ts
+++ src/features/auth/api/logout/api.ts
@@ -1,18 +1,11 @@
-import {NextResponse} from "next/server";
-
 export const logoutUserApi = async (): Promise<Response> => {
 
-    const logoutResponse = await fetch(`${origin}/api/auth/logout-user`, {
+    return await fetch(`${origin}/api/auth/logout-user`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         credentials: 'include', // Ensure cookies are sent for refresh request
-    });
-    if (!logoutResponse.ok) {
-        console.error('Server-side logout-user failed:', logoutResponse.status);
-        return NextResponse.json({message: 'Internal server error during logout-user'}, {status: logoutResponse.status});
-    }
-    return logoutResponse
+    })
 
 };
